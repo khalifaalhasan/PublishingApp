2939f1ecaba324d55e13ecda64d50b3a116b2b76
khalifaalhasan <khlfaalhsn5@gmail.com>
2026-09-10 16:16:43 +0000

    chore: add vercel adpater for preview deployment

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter({
+			out: 'build'
+		}),
 		alias: {
 			'@repo/ui': '../../packages/ui/src/lib/index.ts'
 		}
