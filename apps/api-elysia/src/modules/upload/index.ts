f75ce22a2b5f366dceffd812252d3f64a03df073
khalifaalhasan <khlfaalhsn5@gmail.com>
2026-09-18 00:00:57 +0000

    feat: add minio/presign url config & endpoint

---
+++ apps/api-elysia/src/modules/upload/index.ts
@@ -0,0 +1,20 @@
+import { minioClient } from "@/common/config/minio";
+import Elysia from "elysia";
+
+export const uploadModule = new Elysia({ prefix: "/api/upload" }).get(
+  ":filename",
+  async ({ params }) => {
+    const url = await minioClient.presignedGetObject(
+      process.env["MINIO_BUCKET_NAME"] || "assets",
+      params.filename,
+      24 * 60 * 60,
+    );
+    return { url };
+  },
+  {
+    detail: {
+      tags: ["Upload"],
+      summary: "Get presigned URL for file",
+    },
+  },
+);
