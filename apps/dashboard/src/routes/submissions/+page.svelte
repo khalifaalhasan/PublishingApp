9c9fb206b8227fbe7acb4efe584c9213be0949be
khalifaalhasan <khlfaalhsn5@gmail.com>
2026-09-18 03:47:49 +0000

    chore: add submissions blank page

---
+++ apps/dashboard/src/routes/submissions/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<main class="flex flex-col items-center justify-center gap-2 p-4" style="height: 100vh;">
+  <h1 class="text-3xl font-bold">Submissions</h1>
+  <p class="text-muted-foreground">This is the submissions page.</p>
+</main>
